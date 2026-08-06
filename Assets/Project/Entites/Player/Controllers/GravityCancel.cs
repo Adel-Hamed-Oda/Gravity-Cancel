19048686765f0b7f13eaa48a8a858480068f2c34
Adel Hamed <adel.hamed.oda@gmail.com>
2026-08-06 16:38:21 +0300

    Added the 6th tutorial for gravity cancelling and made gravity cancelling affect all but the player

--- Assets/Project/Entites/Player/Controllers/GravityCancel.cs
+++ Assets/Project/Entites/Player/Controllers/GravityCancel.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 
 public class GravityCancel : MonoBehaviour
 {
-    [SerializeField] private SpeedLimitController speedLimitController;
     [SerializeField] private GameObject gravityCancelExplosion;
     [SerializeField] private GameObject gravityReturnImplosion;
 
@@ -30,12 +29,6 @@ public class GravityCancel : MonoBehaviour
         GravityManager.Instance.DisableGravity();
         gravityCancelCooldownTimer = 9999; // IDK why but it's funny
 
-        maxUpSpeedBeforeCancel = speedLimitController.maxUpSpeed;
-        maxDownSpeedBeforeCancel = speedLimitController.maxDownSpeed;
-
-        speedLimitController.maxUpSpeed = 2f;
-        speedLimitController.maxDownSpeed = 2f;
-
         Instantiate(gravityCancelExplosion, transform);
 
         StartCoroutine(ReturnGravity());
@@ -47,9 +40,6 @@ public class GravityCancel : MonoBehaviour
         Instantiate(gravityReturnImplosion, transform);
 
         yield return new WaitForSeconds(1f);
-
-        speedLimitController.maxUpSpeed = maxUpSpeedBeforeCancel;
-        speedLimitController.maxDownSpeed = maxDownSpeedBeforeCancel;
 
         gravityCancelCooldownTimer = gravityCancelCooldown;
         GravityManager.Instance.EnableGravity();
--- Assets/Project/Managers/GravityManager.cs
+++ Assets/Project/Managers/GravityManager.cs
@@ -1,28 +1,67 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class GravityManager : Manager<GravityManager>
 {
-    private Vector3 originalGravtiy;
-
     [HideInInspector] public bool IsGravityEnabled = true;
+
+    // We store the original gravity scale of each object so we can restore it accurately
+    // (in case some objects originally had a gravity scale of 0.5, 2, etc.)
+    private Dictionary<Rigidbody2D, float> originalGravityScales = new();
 
     protected override void Awake()
     {
         base.Awake();
-
-        originalGravtiy = Physics.gravity;
-        IsGravityEnabled = true;
-    }
-
-    public void EnableGravity()
-    {
-        Physics2D.gravity = originalGravtiy;
         IsGravityEnabled = true;
     }
 
     public void DisableGravity()
     {
-        Physics2D.gravity = new Vector2(0, 0);
+        if (!IsGravityEnabled) return;
+
+        // Find all 2D Rigidbodies currently in the scene
+        Rigidbody2D[] allRigidbodies = FindObjectsByType<Rigidbody2D>(FindObjectsSortMode.None);
+
+        foreach (Rigidbody2D rb in allRigidbodies)
+        {
+            // Skip the player
+            if (rb.CompareTag("Player")) continue;
+
+            // Save the original scale if we haven't seen this object before
+            if (!originalGravityScales.ContainsKey(rb))
+            {
+                originalGravityScales.Add(rb, rb.gravityScale);
+            }
+
+            // Disable gravity for this specific object
+            rb.gravityScale = 0f;
+        }
+
         IsGravityEnabled = false;
+    }
+
+    public void EnableGravity()
+    {
+        if (IsGravityEnabled) return;
+
+        // Find all 2D Rigidbodies currently in the scene
+        Rigidbody2D[] allRigidbodies = FindObjectsByType<Rigidbody2D>(FindObjectsSortMode.None);
+
+        foreach (Rigidbody2D rb in allRigidbodies)
+        {
+            if (rb.CompareTag("Player")) continue;
+
+            // Restore original gravity scale if we have it saved, otherwise default to 1
+            if (originalGravityScales.ContainsKey(rb))
+            {
+                rb.gravityScale = originalGravityScales[rb];
+            }
+            else
+            {
+                rb.gravityScale = 1f;
+            }
+        }
+
+        IsGravityEnabled = true;
     }
 }
